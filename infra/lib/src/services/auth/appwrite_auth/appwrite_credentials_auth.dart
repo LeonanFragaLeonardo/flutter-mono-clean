0256d96e6aec96769d4d8b75e58036f9a034ae46
Leonan <leonan@alunos.utfpr.edu.br>
2022-03-07 10:58:22 -0300

    feat: create appwrite auth with credentials

--- infra/lib/src/services/auth/appwrite_auth/appwrite_credentials_auth.dart
+++ infra/lib/src/services/auth/appwrite_auth/appwrite_credentials_auth.dart
@@ -1,1 +1,26 @@
-class AppWriteCredentialsAuth {}
+import 'package:appwrite/appwrite.dart';
+import 'package:domain/domain.dart';
+import 'package:infra/src/services/auth/appwrite_auth/appwrite_config.dart';
+
+class AppWriteCredentialsAuth implements AuthenticateByPasswordService {
+  final Client client;
+
+  AppWriteCredentialsAuth({required this.client});
+
+  @override
+  Future<AuthenticationOutput> passwordAuthentication(
+      {required String username, required String password}) async {
+    final Account appWriteAccount = Account(client);
+    try {
+      final result = await appWriteAccount.createSession(
+          email: username, password: password);
+      return AuthenticationOutput(
+          authToken: result.data.toString(), idToken: result.data.toString());
+    } on AppwriteException catch (ae) {
+      print(ae.code);
+      print(ae.message);
+      print(ae.response);
+      throw new InvalidCredentialsException();
+    }
+  }
+}
